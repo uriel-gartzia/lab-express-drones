52092e4e8fabd60575b1dcab6465f597c0bf1de1
Uriel Gartzia <ugartziasola@gmail.com>
2023-08-07 19:48:52 +0200

    iteraciones hechas sin bonus

--- routes/drones.js
+++ routes/drones.js
@@ -9,7 +9,7 @@ router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
   Drone.find()
-    .select({ name: 1 })
+    
     .then((response) => {
       console.log(response);
       res.render("./drones/list.hbs", {
@@ -45,19 +45,53 @@ router.post("/drones/create/", (req, res, next) => {
     });
 });
 
-router.get("/drones/:id/edit", (req, res, next) => {
+router.get("/drones/:id/edit", async (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  try {
+    const oneDrone = await Drone.findById(req.params.id)
+    res.render("drones/update-form.hbs", {
+     oneDrone
+    })
+  } catch(error){
+    next(error)
+  }
+
 });
 
-router.post("/drones/:id/edit", (req, res, next) => {
+router.post("/drones/:id/edit", async (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  console.log(req.body)
+  const droneId = req.params.id
+  const {name, propellers, maxSpeed} = req.body
+  try {
+    await Drone.findByIdAndUpdate(droneId, {
+      name: name,
+      propellers: propellers,
+      maxSpeed: maxSpeed
+    })
+    res.redirect("/drones")
+    
+  } catch(error){
+    next(error)
+  }
+
+
 });
 
-router.post("/drones/:id/delete", (req, res, next) => {
+router.post("/drones/:id/delete", async (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  try {
+    await Drone.findByIdAndDelete(req.params.id)
+    console.log(req.params.id)
+    console.log("Pedro: hemos borrado el drone")
+    res.redirect("/drones")
+  } catch (error) {
+    next(error)
+  }
+  
 });
 
 module.exports = router;
